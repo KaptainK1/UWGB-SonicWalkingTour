fcfdf8201d89c8ecbdc863b227caf951cdddf741
Dylan Hoffman <jkioicefire@gmail.com>
2020-04-25 22:45:13 -0500

    added launcher icons for iOS, modified android icons

--- SonicWalkingTour/SonicWalkingTour.Android/obj/Debug/android/environment.x86_64.s
+++ SonicWalkingTour/SonicWalkingTour.Android/obj/Debug/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"213d6ad5-6ad1-45ac-9f8c-b6ad3847bc77"
+	.asciz	"3d0285ea-6567-4cc2-8e0f-eef9058d38c9"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
