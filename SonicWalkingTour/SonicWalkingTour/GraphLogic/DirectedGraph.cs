6c7268f086dd8ea23c68cd0438c2ef51c5c3c249
Dylan Hoffman <jkioicefire.gmail.com>
2020-06-12 12:00:26 -0500

    added working files for a directed graph

---
+++ SonicWalkingTour/SonicWalkingTour/GraphLogic/DirectedGraph.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SonicWalkingTour.GraphLogic
+{
+    class DirectedGraph
+    {
+        private string route;
+        private List< List <Vertex>> startingNodes;
+
+        public DirectedGraph(int numberOfStartingNodes)
+        {
+            init(numberOfStartingNodes);
+        }
+
+        public DirectedGraph()
+        {
+            init(5);
+        }
+
+        void iterate()
+        {
+            for (int i = 0; i < startingNodes.Count; i++)
+            {
+                for (int j = 0; j < startingNodes.ElementAt(i).Count; j++)
+                {
+                    Console.WriteLine(startingNodes.ElementAt(i).ElementAt(j));
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        //method to add an edge to the end of a route for a starting node
+        int addEdge(string vertex, string edge, string routeName)
+        {
+            int _startingSpot;
+            int _edge;
+
+            bool vertexSuccess = int.TryParse(vertex, out _startingSpot);
+            bool edgeSuccess = int.TryParse(edge, out _edge);
+
+            if (vertexSuccess == false || edgeSuccess == false)
+            {
+                Console.WriteLine("Error with starting spot or edge value, please try again");
+                return 1;
+            }
+
+            if(_startingSpot > this.startingNodes.Count() || _startingSpot < 0)
+            {
+                Console.WriteLine("Error with inserting vertex " + _startingSpot + " As this starting route doesn't exist!");
+                return 1;
+            }
+
+            if (_edge < 0 || _edge > Int32.MaxValue)
+            {
+                Console.WriteLine("Edge is not a valid Number!");
+                return 1;
+            }
+
+            Vertex vertex1 = new Vertex(_edge, routeName);
+            this.startingNodes.ElementAt(_startingSpot).Add(vertex1);
+
+            return 0;
+        }
+
+        //method to initialze the nested list, each number passed in will represent a different route
+        void init(int numberOfStartingNodes)
+        {
+            for (int i = 0; i < numberOfStartingNodes; i++)
+            {
+                this.startingNodes.Add(new List<Vertex>());
+            }
+
+        }
+
+        //method to add a new route set
+        void addNewRoute()
+        {
+            this.startingNodes.Add(new List<Vertex>());
+        }
+
+
+
+
+    }
+}
---
+++ SonicWalkingTour/SonicWalkingTour/GraphLogic/Vertex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SonicWalkingTour.GraphLogic
+{
+
+    class Vertex
+    {
+        private int routeID;
+        private string stopName;
+        
+        public Vertex()
+        {
+            setRouteID(0);
+            setStopName("Main Route");
+        }
+
+        public Vertex(int routeID, string stopName)
+        {
+            setRouteID(routeID);
+            setStopName(stopName);
+        }
+
+        void setStopName(string stopName)
+        {
+            this.stopName = stopName;
+        }
+
+        void setRouteID(int routeID)
+        {
+            this.routeID = routeID;
+        }
+
+        int getRouteID()
+        {
+            return this.routeID;
+        }
+
+        string getStopName()
+        {
+            return this.stopName;
+        }
+
+    }
+}
